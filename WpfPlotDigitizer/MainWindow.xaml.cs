bf52fd3229a5cce709d3bf1b489f8f49f1d22a88
Alex Chuang <1392alex1392@gmail.com>
2018-10-05 02:29:49 +0100

    openfilecommand

--- WpfPlotDigitizer/MainWindow.xaml.cs
+++ WpfPlotDigitizer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MahApps.Metro.Controls;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ WpfPlotDigitizer/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CycWpfLibrary.MVVM;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfPlotDigitizer
+{
+  public class MainWindowViewModel : ViewModelBase
+  {
+    public MainWindowViewModel()
+    {
+      OpenFileCommand = new RelayCommand(OpenFile);
+    }
+
+    public ICommand OpenFileCommand { get; set; } //bind with button
+    public Uri ImageSource { get; set; } // bind with image
+    public void OpenFile()
+    {
+      var dialog = new OpenFileDialog();
+      dialog.Filter = "Image | *.png; *.jpg;";
+      if (dialog.ShowDialog() == false)
+      {
+        return;
+      }
+      ImageSource = new Uri(dialog.FileName);
+    }
+  }
+}
