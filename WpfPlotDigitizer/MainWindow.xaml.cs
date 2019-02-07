74f564fb3c509f120837f4ea144b4a8bf7b276b7
Alex Chuang <1392alex1392@gmail.com>
2019-02-07 05:23:52 +0000

    Add popupWindow

--- WpfPlotDigitizer/MainWindow.xaml.cs
+++ WpfPlotDigitizer/MainWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using MahApps.Metro.Controls;
+﻿using CycWpfLibrary;
+using CycWpfLibrary.CustomControls;
+using CycWpfLibrary.Media;
+using CycWpfLibrary.Resources;
+using MahApps.Metro.Controls;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using static WpfPlotDigitizer.DI;
 
 namespace WpfPlotDigitizer
@@ -13,6 +22,42 @@ namespace WpfPlotDigitizer
       InitializeComponent();
 
       DataContext = mainWindowVM;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      switch ((ApplicationPages)pageManager.Index)
+      {
+        case ApplicationPages.Browse:
+          new PopupWindow
+          {
+            PlacementTarget = browsePage.browseButton,
+            Text = "Click the button to browse your image!",
+          }.ShowDialog();
+          new PopupWindow
+          {
+            PlacementTarget = browsePage.dropBorder,
+            Text = "Or you can simply drag and drop your image onto this area!",
+          }.ShowDialog();
+
+          break;
+        case ApplicationPages.AxLim:
+          break;
+        case ApplicationPages.Axis:
+          break;
+        case ApplicationPages.Filter:
+          break;
+        case ApplicationPages.Erase:
+          break;
+        case ApplicationPages.Data:
+          break;
+        case ApplicationPages.Save:
+          break;
+        case ApplicationPages.NumOfPages:
+          break;
+        default:
+          break;
+      }
     }
   }
 }
--- WpfPlotDigitizer/MainWindowVM.cs
+++ WpfPlotDigitizer/MainWindowVM.cs
@@ -1,12 +1,31 @@
 ﻿using CycWpfLibrary;
 using CycWpfLibrary.Controls;
 using CycWpfLibrary.MVVM;
+using CycWpfLibrary.Resources;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
 using static WpfPlotDigitizer.DI;
+using MahApps.Metro;
 
 namespace WpfPlotDigitizer
 {
   public class MainWindowVM : ViewModelBase
   {
-    public PageManagerBase PageManager => appManager.PageManager;
+    public MainWindowVM()
+    {
+      QuestionCommand = new RelayCommand(Question);
+    }
+
+    public PageManagerBase PageManager => pageManager;
+
+    public ICommand QuestionCommand { get; set; }
+    private void Question()
+    {
+      
+    }
   }
 }
