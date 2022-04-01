3b67d32294a152aa7c1a6d2da2821e63433d3c2a
YXC826 <YXC826@student.bham.ac.uk>
2022-04-01 11:47:14 +0100

    fix back/next command not updating bug

--- PlotDigitizer.App/View/Utilities/PageManager.cs
+++ PlotDigitizer.App/View/Utilities/PageManager.cs
@@ -7,93 +7,102 @@ using System.Windows.Controls;
 
 namespace PlotDigitizer.App
 {
-	public class PageManager : INotifyPropertyChanged
-	{
-		public event PropertyChangedEventHandler PropertyChanged;
+    public class PageManager : INotifyPropertyChanged
+    {
+        private int pageIndex = 0;
 
-		/// <summary>
-		/// Public read/write access to allow two-way binding 
-		/// </summary>
-		public int PageIndex { get; set; } = 0; 
+        public event PropertyChangedEventHandler PropertyChanged;
 
-		public List<Page> PageList { get; private set; }
+        /// <summary>
+        /// Public read/write access to allow two-way binding 
+        /// </summary>
+        public int PageIndex
+        {
+            get => pageIndex;
+            set
+            {
+                if (value != pageIndex)
+                {
+                    pageIndex = value;
+                    BackCommand.RaiseCanExecuteChanged();
+                    NextCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+        public List<Page> PageList { get; private set; }
 
-		public Page CurrentPage => PageList[PageIndex];
+        public Page CurrentPage => PageList[PageIndex];
 
-		public RelayCommand BackCommand { get; private set; }
+        public RelayCommand BackCommand { get; private set; }
 
-		public RelayCommand NextCommand { get; private set; }
+        public RelayCommand NextCommand { get; private set; }
 
-		public RelayCommand<int> GoToCommand { get; private set; }
+        public RelayCommand<int> GoToCommand { get; private set; }
 
-		public RelayCommand<Type> GoToByTypeCommand { get; private set; }
+        public RelayCommand<Type> GoToByTypeCommand { get; private set; }
 
-		private PageManager()
-		{
-			BackCommand = new RelayCommand(GoBack, CanGoBack);
-			NextCommand = new RelayCommand(GoNext, CanGoNext);
-			GoToCommand = new RelayCommand<int>(GoTo, CanGoTo);
-			GoToByTypeCommand = new RelayCommand<Type>(GoTo, CanGoTo);
-		}
+        private PageManager()
+        {
+            BackCommand = new RelayCommand(GoBack, CanGoBack);
+            NextCommand = new RelayCommand(GoNext, CanGoNext);
+            GoToCommand = new RelayCommand<int>(GoTo, CanGoTo);
+            GoToByTypeCommand = new RelayCommand<Type>(GoTo, CanGoTo);
+        }
 
-		public PageManager(List<Page> pageList) : this()
-		{
-			PageList = pageList;
-		}
+        public PageManager(List<Page> pageList) : this()
+        {
+            PageList = pageList;
+        }
 
-		public Page GetPage(Type TPage)
-		{
-			return PageList.FirstOrDefault(p => p.GetType() == TPage);
-		}
+        public Page GetPage(Type TPage)
+        {
+            return PageList.FirstOrDefault(p => p.GetType() == TPage);
+        }
 
-		public TPage GetPage<TPage>() where TPage : class
-		{
-			return PageList.FirstOrDefault(p => p is TPage) as TPage;
-		}
+        public TPage GetPage<TPage>() where TPage : class
+        {
+            return PageList.FirstOrDefault(p => p is TPage) as TPage;
+        }
 
-		private void GoBack()
-		{
-			PageIndex--;
-			BackCommand.RaiseCanExecuteChanged();
-			NextCommand.RaiseCanExecuteChanged();
-		}
+        private void GoBack()
+        {
+            PageIndex--;
+        }
 
-		private bool CanGoBack()
-		{
-			return PageIndex > 0;
-		}
+        private bool CanGoBack()
+        {
+            return PageIndex > 0;
+        }
 
-		private void GoNext()
-		{
-			PageIndex++;
-			BackCommand.RaiseCanExecuteChanged();
-			NextCommand.RaiseCanExecuteChanged();
-		}
+        private void GoNext()
+        {
+            PageIndex++;
+        }
 
-		private bool CanGoNext()
-		{
-			return PageIndex < PageList.Count - 1;
-		}
+        private bool CanGoNext()
+        {
+            return PageIndex < PageList.Count - 1;
+        }
 
-		private void GoTo(int targetIndex)
-		{
-			PageIndex = targetIndex;
-		}
+        private void GoTo(int targetIndex)
+        {
+            PageIndex = targetIndex;
+        }
 
-		private bool CanGoTo(int targetIndex)
-		{
-			return targetIndex >= 0 && targetIndex < PageList.Count;
-		}
+        private bool CanGoTo(int targetIndex)
+        {
+            return targetIndex >= 0 && targetIndex < PageList.Count;
+        }
 
-		private void GoTo(Type TPage)
-		{
-			var index = PageList.FindIndex(p => p.GetType() == TPage);
-			PageIndex = index;
-		}
+        private void GoTo(Type TPage)
+        {
+            var index = PageList.FindIndex(p => p.GetType() == TPage);
+            PageIndex = index;
+        }
 
-		private bool CanGoTo(Type TPage)
-		{
-			return PageList.Any(p => p.GetType() == TPage);
-		}
-	}
+        private bool CanGoTo(Type TPage)
+        {
+            return PageList.Any(p => p.GetType() == TPage);
+        }
+    }
 }
