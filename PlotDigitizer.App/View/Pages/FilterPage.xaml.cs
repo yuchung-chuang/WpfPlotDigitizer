40e1243c127cfdaa07bedc08eac72ed77e059a67
Alex Chuang <1392alex1392@gmail.com>
2022-04-02 16:12:45 +0100

    add disabled + fix On_Property_Changed bug

--- PlotDigitizer.App/View/Pages/FilterPage.xaml.cs
+++ PlotDigitizer.App/View/Pages/FilterPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Emgu.CV;
 using Emgu.CV.Structure;
 using PlotDigitizer.Core;
+using PropertyChanged;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,25 +9,28 @@ using System.Windows.Media;
 
 namespace PlotDigitizer.App
 {
-	/// <summary>
-	/// Interaction logic for FilterPage.xaml
-	/// </summary>
-	public partial class FilterPage : Page, INotifyPropertyChanged
+	[AddINotifyPropertyChangedInterface]
+	public partial class FilterPage : Page
 	{
 		private readonly Model model;
-
-		public double MinR { get; set; } 
-		public double MaxR { get; set; } 
-		public double MinG { get; set; } 
-		public double MaxG { get; set; } 
-		public double MinB { get; set; } 
-		public double MaxB { get; set; } 
+		public bool IsDisabled => model.CroppedImage is null;
+		
+		[OnChangedMethod(nameof(OnMinRChanged))]
+		public double MinR { get; set; }
+		[OnChangedMethod(nameof(OnMaxRChanged))]
+		public double MaxR { get; set; }
+		[OnChangedMethod(nameof(OnMinGChanged))]
+		public double MinG { get; set; }
+		[OnChangedMethod(nameof(OnMaxGChanged))]
+		public double MaxG { get; set; }
+		[OnChangedMethod(nameof(OnMinBChanged))]
+		public double MinB { get; set; }
+		[OnChangedMethod(nameof(OnMaxBChanged))]
+		public double MaxB { get; set; }
 		public ImageSource ImageSource => Image?.ToBitmapSource();
 
 		public Image<Rgba, byte> Image { get; private set; }
 		public Image<Rgba, byte> CroppedImage { get; private set; }
-
-		public event PropertyChangedEventHandler PropertyChanged;
 
 		public FilterPage()
 		{
@@ -42,6 +46,10 @@ namespace PlotDigitizer.App
 
 		private void FilterPage_Loaded(object sender, RoutedEventArgs e)
 		{
+			IsEnabled = !IsDisabled;
+			if (IsDisabled) {
+				return;
+			}
 			CroppedImage = model.CroppedImage;
 			MinR = model.FilterMin.Red;
 			MinG = model.FilterMin.Green;
@@ -50,33 +58,33 @@ namespace PlotDigitizer.App
 			MaxG = model.FilterMax.Green;
 			MaxB = model.FilterMax.Blue;
 			FilterImage();
-			PropertyChanged += FilterPage_PropertyChanged;
 		}
 
 		private void FilterPage_Unloaded(object sender, RoutedEventArgs e)
 		{
+			if (IsDisabled) {
+				return;
+			}
 			model.FilterMin = new Rgba(MinR, MinG, MinB, byte.MaxValue);
 			model.FilterMax = new Rgba(MaxR, MaxG, MaxB, byte.MaxValue);
 			model.FilterImage();
 		}
-
-		private void FilterPage_PropertyChanged(object sender, PropertyChangedEventArgs e)
-		{
-			if ((nameof(MinR) + nameof(MaxR) +
-				nameof(MinG) + nameof(MaxG) +
-				nameof(MinB) + nameof(MaxB)).Contains(e.PropertyName)) {
-				FilterImage();
-			}
-		}
-
 		private void FilterImage()
 		{
-			if (CroppedImage is null) {
+			if (IsDisabled) {
 				return;
 			}
 			var min = new Rgba(MinR, MinG, MinB, byte.MaxValue);
 			var max = new Rgba(MaxR, MaxG, MaxB, byte.MaxValue);
 			Image = Methods.FilterRGB(CroppedImage, min, max);
 		}
+
+		private void OnMinRChanged() => FilterImage();
+		private void OnMinGChanged() => FilterImage();
+		private void OnMinBChanged() => FilterImage();
+		private void OnMaxRChanged() => FilterImage();
+		private void OnMaxGChanged() => FilterImage();
+		private void OnMaxBChanged() => FilterImage();
+
 	}
 }
