74a8ab318e4cda1af07e5397521f7e56aef1a8fb
Alex Chuang <1392alex1392@gmail.com>
2022-04-19 22:32:24 +0100

    move autopageturner to core services

---
+++ PlotDigitizer.Core/Services/AutoPageTurner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PlotDigitizer.Core
+{
+	public class AutoPageTurner
+	{
+		private readonly PageManager pageManager;
+
+		public AutoPageTurner(MainWindowViewModel mainWindowViewModel, LoadPageViewModel viewModel)
+		{
+			pageManager = mainWindowViewModel.PageManager;
+			viewModel.NextPage += LoadPage_NextPage;
+		}
+
+		private void LoadPage_NextPage(object sender, EventArgs e)
+		{
+			if (pageManager.NextCommand.CanExecute()) {
+				pageManager.NextCommand.Execute();
+			}
+		}
+
+	}
+}
