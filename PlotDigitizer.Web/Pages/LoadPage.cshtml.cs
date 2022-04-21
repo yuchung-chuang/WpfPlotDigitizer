e6c7c730271a0d989801f27ecc4616ea6c096f3c
Alex Chuang <1392alex1392@gmail.com>
2022-04-21 01:07:08 +0100

    add web project

---
+++ PlotDigitizer.Web/Pages/LoadPage.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlotDigitizer.Web.Pages
+{
+    public class LoadPageModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(IFormFile formFile)
+        {
+            var downloadPath = Path.GetTempFileName();
+            using var stream = new FileStream(downloadPath, FileMode.OpenOrCreate);
+            await formFile.CopyToAsync(stream);
+
+            return RedirectToPage("AxisLimitPage");
+        }
+    }
+}
