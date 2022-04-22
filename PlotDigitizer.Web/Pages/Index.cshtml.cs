1f170e1d0a553f8dffc3229eee40b60351f48e88
Alex Chuang <1392alex1392@gmail.com>
2022-04-22 22:40:22 +0100

    achieve image loading without page refreshing using ajax + partial page

--- PlotDigitizer.Web/Pages/Index.cshtml.cs
+++ PlotDigitizer.Web/Pages/Index.cshtml.cs
@@ -12,8 +12,21 @@ namespace PlotDigitizer.Web.Pages
 			_logger = logger;
 		}
 
+		public string Message { get; set; }
+
 		public void OnGet()
 		{
+			Message = "OnGet executed";
 		}
+
+		public void OnPost()
+		{
+			Message = "OnPost executed";
+		}
+
+		public void OnPostType1() => Message = "Type1 posted";
+		public void OnPostType2() => Message = "Type2 posted";
+
+		//public int OnPost(int number1, int number2) => number1 + number2;
 	}
 }
--- PlotDigitizer.Web/Pages/LoadPage.cshtml.cs
+++ PlotDigitizer.Web/Pages/LoadPage.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.StaticFiles;
 using PlotDigitizer.Web.Models;
 
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Drawing;
 using System.IO;
 using System.Threading.Tasks;
@@ -23,17 +24,22 @@ namespace PlotDigitizer.Web.Pages
 			ImageModel = model;
 		}
 
+		[BindProperty, Display(Name = "File")]
+		public IFormFile UploadedFile { get; set; }
 
-		public void OnGet()
+		public IActionResult OnGetImage()
 		{
+			return Partial("_LoadedImage", ImageModel.InputImageSource);
 		}
 
-		public async Task<IActionResult> OnPostAsync(IFormFile formFile)
+		public async Task<IActionResult> OnPostAsync()
 		{
-			ImageModel.InputImage = await formFile.ToImageAsync();
-			ImageModel.InputImageSource = await formFile.ToImgSrcAsync();
-			return RedirectToPage("AxisLimitPage");
-			//return Page();
+			//ImageModel.InputImage = await formFile.ToImageAsync();
+			//ImageModel.InputImageSource = await formFile.ToImgSrcAsync();
+			//return RedirectToPage("AxisLimitPage");
+			ImageModel.InputImage = await UploadedFile.ToImageAsync();
+			ImageModel.InputImageSource = await UploadedFile.ToImgSrcAsync();
+			return Page();
 		}
 	}
 }
