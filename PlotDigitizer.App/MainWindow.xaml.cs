f41d22c26a0016bd07f67fec3144830ad5957c4f
Alex Chuang <1392alex1392@gmail.com>
2022-11-13 04:26:08 +0000

    fix backspace navigation issue

--- PlotDigitizer.App/MainWindow.xaml.cs
+++ PlotDigitizer.App/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace PlotDigitizer.App
 {
@@ -7,6 +8,23 @@ namespace PlotDigitizer.App
 		public MainWindow()
 		{
 			InitializeComponent();
+			Loaded += MainWindow_Loaded;
+		}
+
+		private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			foreach (KeyGesture gesture in NavigationCommands.BrowseBack.InputGestures) {
+				if (gesture.Key == Key.Back) {
+					NavigationCommands.BrowseBack.InputGestures.Remove(gesture);
+					break;
+				}
+			}
+			foreach (KeyGesture gesture in NavigationCommands.BrowseForward.InputGestures) {
+				if (gesture.Key == Key.Back) {
+					NavigationCommands.BrowseForward.InputGestures.Remove(gesture);
+					break;
+				}
+			}
 		}
 	}
 }
