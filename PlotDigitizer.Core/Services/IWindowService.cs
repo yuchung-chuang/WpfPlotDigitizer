8b2916728b105e972646d83fd52289e32cc5f77d
ALEX_P15V <yuchung.chuang@gmail.com>
2024-09-21 02:05:45 +0100

    [Attach] bug fix: editor depends on mainwindow, which is not set correctly when using viewmodel first approach.

--- PlotDigitizer.Core/Services/IWindowService.cs
+++ PlotDigitizer.Core/Services/IWindowService.cs
@@ -5,11 +5,8 @@ namespace PlotDigitizer.Core
 	public interface IWindowService
 	{
 		bool CloseDialog(ViewModelBase viewModel, bool dialogResult);
-		void CloseSplashWindow();
 		bool CloseWindow(ViewModelBase viewModel);
 		bool? ShowDialog(ViewModelBase viewModel);
-		void ShowMainWindow(ViewModelBase viewModel);
-		void ShowSplashWindow();
 		void ShowWindow(ViewModelBase viewModel);
 	}
 }
