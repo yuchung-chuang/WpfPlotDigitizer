d85361e437951eef11e08a75a17dc47a2d377aa6
ALEX_P15V <yuchung.chuang@gmail.com>
2024-09-25 19:02:12 +0100

    add blazor project and implement load page with objectURL as imageURL

---
+++ PlotDigitizer.Blazor/PlotDigitizer.Blazor.Client/Pages/Load.razor.js
@@ -0,0 +1,5 @@
+﻿window.createObjectURL = function (inputFile) {
+	const url = URL.createObjectURL(inputFile.files[0]);
+	window.addEventListener('unload', () => URL.revokeObjectURL(url), { once: true });
+	return url;
+};
