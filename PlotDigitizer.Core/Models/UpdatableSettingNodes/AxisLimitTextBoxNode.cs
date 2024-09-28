04f915df7a004ee20a69048f8a4a6e13545e166b
ALEX_P15V <yuchung.chuang@gmail.com>
2024-09-29 00:49:32 +0100

    Add AxisLimitTextBoxNode

---
+++ PlotDigitizer.Core/Models/UpdatableSettingNodes/AxisLimitTextBoxNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlotDigitizer.Core
+{
+    public class AxisLimitTextBoxNode : UpdatableNode<AxisLimitTextBox>
+    {
+        private readonly AxisLocationNode axisLocation;
+
+        public AxisLimitTextBoxNode(AxisLocationNode axisLocation)
+        {
+            this.axisLocation = axisLocation;
+            axisLocation.Updated += (s, e) => OnOutdated();
+            axisLocation.Outdated += (s, e) => OnOutdated();
+        }
+
+        public override void Update()
+        {
+            if (!axisLocation.CheckUpdate())
+                return;
+            Value = default;
+            base.OnUpdated();
+        }
+    }
+}
