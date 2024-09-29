f8e3663b7ad3176a7ce571e1681118a9aa0803e0
ALEX_P15V <yuchung.chuang@gmail.com>
2024-09-29 23:47:07 +0100

    fixed interactivity of selectionBox by extending the margin

---
+++ PlotDigitizer.WPF/Utilities/ValueConverters/ExtendMarginConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PlotDigitizer.WPF
+{
+    public class ExtendMarginConverter : MultiValueConverterBase<ExtendMarginConverter>
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2) {
+                return null;
+            }
+            if (values[0] is not Thickness boxMargin
+                || values[1] is not double boxThickness) {
+                return null;
+            }
+            return new Thickness(boxMargin.Left - boxThickness, boxMargin.Top - boxThickness, 0, 0);
+        }
+
+        public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
